fbb650f04a11b740cbc38e941c2e53bd9ebdba4e
Nikola Djikanovic <nikola01djikanovic@gmail.com>
2022-07-21 18:20:17 +0200

    Novi Update

--- Web_Auto/Program.cs
+++ Web_Auto/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 /*
- 
+ ne radi
 var service = new ServiceCollection();
 service.AddScoped<ICar, Car>();
 IServiceProvider provider = service.BuildServiceProvider();
@@ -15,6 +15,12 @@ provider.GetRequiredService<ICar>();
 
  */
 
+var service = builder.Services.AddScoped<ICar,Car>();
+var ervice1 = builder.Services.AddScoped<IImmobilizer, Immobilizer>();
+
+IServiceProvider provider = service.BuildServiceProvider();
+var ccar = provider.GetRequiredService<ICar>();
+var carr = provider.GetRequiredService<IImmobilizer>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
